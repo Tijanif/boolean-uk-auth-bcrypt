b35916d3185767d16e47d868a48ac4b6b2fc1f1a
Tijani Fulani <Fulani07@gmail.com>
2021-08-24 16:18:05 +0100

    routes for auth

--- src/auth/router.ts
+++ src/auth/router.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
+import { createAUser } from '../resources/users/controller';
 
-import { loginUser } from './controller';
+import { loginUser, logOutUser } from './controller';
 
 const router = Router();
 
 router.route('/login').post(loginUser);
+
+router.route('/logout').post(logOutUser);
+
+router.route('/signup').post(createAUser);
 
 export default router;
