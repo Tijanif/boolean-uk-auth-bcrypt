dd5be7eef4ea5d90bb9591d476b690fc46b8ca68
Tijani Fulani <Fulani07@gmail.com>
2021-08-24 16:07:33 +0100

    added cors, created a global extention for my current user to the response.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
 import express from 'express';
 import { Request, Response } from 'express';
+import { JwtPayload } from 'jsonwebtoken';
 import usersRouter from './resources/users/router';
 import authRouter from './auth/router';
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+import loginAuth from './middlewares/loginAuth';
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser: string | JwtPayload;
+    }
+  }
+}
+
+const cors = require('cors');
 
 // App initialisation
 const app = express();
@@ -12,11 +24,15 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: 'http://localhost:3001', credentials: true }));
 
 // Routes
 
 // login
 app.use(authRouter);
+
+app.use(loginAuth);
+
 app.use('/users', usersRouter);
 
 app.all('*', (req: Request, res: Response) => {
