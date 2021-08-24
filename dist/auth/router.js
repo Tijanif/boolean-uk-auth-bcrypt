f6f1dd7a13164861d630ecee45c451ace0097f7d
Tijani Fulani <Fulani07@gmail.com>
2021-08-24 16:18:18 +0100

    .

--- dist/auth/router.js
+++ dist/auth/router.js
@@ -1,8 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const controller_1 = require("./controller");
+const controller_1 = require("../resources/users/controller");
+const controller_2 = require("./controller");
 const router = express_1.Router();
-router.route('/login').post(controller_1.loginUser);
+router.route('/login').post(controller_2.loginUser);
+router.route('/logout').post(controller_2.logOutUser);
+router.route('/signup').post(controller_1.createAUser);
 exports.default = router;
 //# sourceMappingURL=router.js.map
